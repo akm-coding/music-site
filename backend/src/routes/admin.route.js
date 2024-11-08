9e52bf5a9c27b627b570266d1ae2c194a27b5dac
Aung Kaung Myat <115795473+Aung-kaung-myat-coding@users.noreply.github.com>
2024-11-08 09:30:12 +0700

    feat: create song, create album, delete song, and delete album routes

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
-import { getAdmin } from "../controllers/admin.controller.js";
+import {
+  checkAdmin,
+  createAlbum,
+  createSong,
+  deleteAlbum,
+  deleteSong,
+} from "../controllers/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", protectRoute, requireAdmin);
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
